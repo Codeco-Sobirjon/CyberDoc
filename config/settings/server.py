e083998e581292ce9dcdd2ce1f3a9dc012b0185b
Codeco-Sobirjon <sobirbobojonov05763104@gmail.com>
2024-11-28 17:53:48 +0500

    chnaged

--- config/settings/server.py
+++ config/settings/server.py
@@ -10,7 +10,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'cyberdoc',
         'USER': 'postgres',
-        "PASSWORD": "0576",
+        "PASSWORD": "1",
         "HOST": "localhost",
         "PORT": 5432,
     }
