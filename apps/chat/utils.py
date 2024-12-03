0f23764ad0ea3ce16e4268f584406754d5d4811c
Codeco-Sobirjon <sobirbobojonov05763104@gmail.com>
2024-12-03 17:01:31 +0500

    chnaged

--- apps/chat/utils.py
+++ apps/chat/utils.py
@@ -1,8 +1,8 @@
 
 
 def custom_user_has_student_role(user):
-    return user.groups.filter(name='Студент').exists()
+    return user.groups.filter(id=2).exists()
 
 
 def custom_user_has_author_role(user):
-    return user.groups.filter(name='Автор').exists()
+    return user.groups.filter(id=1).exists()
